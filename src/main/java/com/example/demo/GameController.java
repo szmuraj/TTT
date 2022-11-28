f4cae33a18da8fb525306873864eaa1f6f512b9c
szmuraj <przemyslaw.sicinski@o2.pl>
2022-11-28 17:27:15 +0100

    1.1

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -156,7 +156,8 @@ public class GameController {
     }
     @FXML
     public void menu() {
-
+    MainController mainController = new MainController();
+    mainController.loadMenuScreen();
     }
 
     @FXML
